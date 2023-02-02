6fd088bbb3da9a0d0b5bb9da5db0de505a890fdc
GihongYim <ygh0410@naver.com>
2023-02-02 15:31:51 +0900

    ray sphere

--- src/rt_engine/ray.c
+++ src/rt_engine/ray.c
@@ -6,7 +6,7 @@
 /*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 19:42:50 by gyim              #+#    #+#             */
-/*   Updated: 2023/02/01 19:52:47 by gyim             ###   ########seoul.kr  */
+/*   Updated: 2023/02/02 15:08:02 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,30 @@ t_rgb	raytracing(t_scene *scnen, int w, int h)
 	black.r = 0.0;
 	black.g = 0.0;
 	black.b = 0.0;
-
-	dx = v3_div_d(scnen->screen.x_dir , (WIN_WIDTH / 2.0));
-	dy = v3_div_d(scnen->screen.y_dir , (WIN_HEIGHT / 2.0));
+	dx = v3_div_d(scnen->screen.x_dir, (WIN_WIDTH / 2.0));
+	dy = v3_div_d(scnen->screen.y_dir, (WIN_HEIGHT / 2.0));
 	dx = v3_mul_d(dx, (double)w);
-	dy = v3_mul_d(dy, (double)h);
+	dy = v3_mul_d(dy, -1.0 * (double)h);
 	temp = v3_plus_v3(scnen->screen.upperleft, dx);
-	screen_point = v3_minus_v3(temp, dy);
-	if (check_sphere(screen_point, scnen->spheres->content))
-		return (((t_sphere *)(scnen->spheres->content))->color);
+	screen_point = v3_plus_v3(temp, dy);
+	if (check_sphere(screen_point, scnen->spheres->next->content))
+		return (((t_sphere *)(scnen->spheres->next->content))->color);
 	else
 		return (black);
 }
 
 int	check_sphere(t_vec3	ray, t_sphere *sphere)
 {
+	double	a;
+	double	b;
+	double	c;
 	double	discriminant;
-	double	term[2];
 
-	term[0] = v3_inner_product_v3(ray, sphere->pos);
-	term[0] = pow(term[0], 2.0);
-	term[1] = v3_inner_product_v3(ray, ray);
-	term[1] = term[1]
-		* (v3_inner_product_v3(sphere->pos, sphere->pos)
-			- pow(sphere->diameter / 2, 2.0));
-	discriminant = term[0] - term[1];
+	a = v3_inner_product_v3(ray, ray);
+	b = -2.0 * v3_inner_product_v3(ray, sphere->pos);
+	c = v3_inner_product_v3(sphere->pos, sphere->pos)
+		- pow(sphere->diameter / 2.0, 2);
+	discriminant = pow(b, 2.0) - 4.0 * a * c;
 	if (discriminant >= 0)
 		return (1);
 	else
