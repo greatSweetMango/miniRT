10a6a7cbe5a8148a21681af72936763cd6ac0a53
greatSweetMango <jhkim0097@gmail.com>
2023-02-26 03:03:17 +0900

    good

--- src/key_hooks/mouse_button.c
+++ src/key_hooks/mouse_button.c
@@ -14,8 +14,16 @@
 
 int	mouse_button(int btn, int x, int y, t_scene *scene)
 {
-	// if (raytracing(scene, x, y))
-	// 	scene->selected_obj = raytracing(scene, x, y);
-	printf("\nBTN: %d\nPOS: (%d, %d)\nSelected_OBJ: %d\n", btn, x, y, scene->selected_obj->obj_type);
+	(void)btn, (void)x, (void)y, (void)scene;
+	// t_ray		ray;
+	// t_hit_info	hit_info;
+
+	// ray = get_ray_camera_to_obj(scene, x, y);
+	// check_objects(ray, scene);
+	// if (hit_info.obj)
+	// {
+	// 	scene->selected_obj = hit_info.obj;
+	// 	printf("\nBTN: %d\nPOS: (%d, %d)\nSelected_OBJ: %d\n", btn, x, y, scene->selected_obj->obj_type);
+	// }
 	return (0);
 }
