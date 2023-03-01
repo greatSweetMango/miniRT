3a2d9a0243b2c026f9b7c5575b467b4bc8afd236
GihongYim <ygh0410@naver.com>
2023-03-01 18:32:26 +0900

    light

--- libft/ft_isalpha.d
+++
@@ -1,5 +0,0 @@
-ft_isalpha.o: ft_isalpha.c libft.h get_next_line.h
-
-libft.h:
-
-get_next_line.h:
--- libft/ft_strnstr.d
+++
@@ -1,5 +0,0 @@
-ft_strnstr.o: ft_strnstr.c libft.h get_next_line.h
-
-libft.h:
-
-get_next_line.h:
