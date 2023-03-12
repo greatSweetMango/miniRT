418b7f84e3a0b9d5ba04ebb2d07c68f947005d18
GihongYim <ygh0410@naver.com>
2023-03-12 17:29:35 +0900

    rotate cylinder modified

--- src/key_hooks/update_cylinder.c
+++ src/key_hooks/update_cylinder.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   update_cylinder.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jaehyuki <jaehyuki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gyim <gyim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 18:25:03 by jaehyuki          #+#    #+#             */
-/*   Updated: 2023/02/20 19:39:02 by jaehyuki         ###   ########.fr       */
+/*   Updated: 2023/03/12 17:28:08 by gyim             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ void	update_cylinder(int keycode, t_scene *scene)
 	cylinder = scene->selected_obj->content;
 	if (keycode == KEY_LEFT || keycode == KEY_RIGHT
 		|| keycode == KEY_DOWN || keycode == KEY_UP)
+	{
 		rotate_obj(keycode, &cylinder->orientation, scene);
+		rotate_obj(keycode, &cylinder->x_axis, scene);
+	}
 	else if (keycode == KEY_A || keycode == KEY_D
 				|| keycode == KEY_W || keycode == KEY_S
 				|| keycode == KEY_R || keycode == KEY_F)
